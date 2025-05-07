5bedccbd3e436987774d98c75996730bbdfe4cb5
Aleks <aleksyankov06@gmail.com>
2025-05-07 16:09:42 +0300

    seventh commit

--- obj/Debug/net8.0/ProektAleks.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProektAleks.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProektAleks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9afb3a38eaf3617eb27fa7d5b9c629b209065801")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6d8cb02adf2296d61cab38c7b6865fed26b7b14")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProektAleks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProektAleks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
