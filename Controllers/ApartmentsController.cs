ea72dca6aed1dca89bca36eb84695d5cfce53d6f
Aleks <aleksyankov06@gmail.com>
2025-05-19 23:29:24 +0300

    twentyone commit

--- Controllers/ApartmentsController.cs
+++ Controllers/ApartmentsController.cs
@@ -20,7 +20,7 @@ namespace ProektAleks.Controllers
             _context = context;
         }
 
-        // GET: Apartmen
+        // GET: Apartments
         public async Task<IActionResult> Index()
         {
             var applicationDbContext = _context.Apartments.Include(a => a.Categories);
